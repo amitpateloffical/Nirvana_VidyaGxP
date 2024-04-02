f7098bf537e286d363b8df1ececf3d1840206912
MK7841 <manish.khatriya@vidyagxp.com>
2024-04-02 10:43:03 +0530

    Date Due Instration

--- resources/views/frontend/New_forms/production-line-audit.blade.php
+++ resources/views/frontend/New_forms/production-line-audit.blade.php
@@ -86,7 +86,8 @@
 
                             <div class="col-md-6 new-date-data-field">
                                 <div class="group-input input-date">
-                                    <label for="due-date">Date Due <span class="text-danger"></span></label>
+                                    <label for="due-date">Date Due</label>
+                                    <div><small class="text-primary">Please mention expected date of completion</small></div>
                                     <div class="calenderauditee">
                                         <input type="text" id="due_date" readonly placeholder="DD-MMM-YYYY" />
                                         <input type="date" name="due_date" min="{{ \Carbon\Carbon::now()->format('Y-m-d') }}" value="" class="hide-input" oninput="handleDateInput(this, 'due_date')" />
