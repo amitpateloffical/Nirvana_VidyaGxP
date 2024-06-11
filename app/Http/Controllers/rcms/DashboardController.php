ee3d85654673a1436a687aed8b6b8c7e525820e1
sonali25sharma <sonalisharmavidyagxp@gmail.com>
2024-06-11 15:48:51 +0530

    link set in dashboard

--- app/Http/Controllers/rcms/DashboardController.php
+++ app/Http/Controllers/rcms/DashboardController.php
@@ -18,6 +18,7 @@ use App\Models\RootCauseAnalysis;
 use App\Models\Observation;
 use App\Models\Deviation;
 use App\Models\MedicalDeviceRegistration;
+use App\Models\DosierDocuments;
 use Helpers;
 use App\Models\User;
 use Carbon\Carbon;
@@ -67,7 +68,7 @@ class DashboardController extends Controller
         $datas12 = Observation::orderByDesc('id')->get();
         $datas13 = Deviation::orderByDesc('id')->get();
         $datas15 = MedicalDeviceRegistration::orderByDesc('id')->get();
-
+        $datas16 = DosierDocuments::orderByDesc('id')->get();
 
         foreach ($datas as $data) {
             $data->create = Carbon::parse($data->created_at)->format('d-M-Y h:i A');
@@ -344,6 +345,26 @@ class DashboardController extends Controller
                 "record" => $data->record,
                 "division_id" => $data->division_id,
                 "type" => "MedicalDeviceRegistration",
+                "parent_id" => $data->parent_id,
+                "parent_type" => $data->parent_type,
+                "short_description" => $data->short_description ? $data->short_description : "-",
+                "initiator_id" => $data->initiator_id,
+                "intiation_date" => $data->intiation_date,
+                "stage" => $data->status,
+                "date_open" => $data->create,
+                "date_close" => $data->updated_at,
+            ]);
+        }
+
+        foreach ($datas16 as $data) {
+            $data->create = Carbon::parse($data->created_at)->format('d-M-Y h:i A');
+
+            array_push($table, [
+                "id" => $data->id,
+                "parent" => $data->parent_record ? $data->parent_record : "-",
+                "record" => $data->record,
+                "division_id" => $data->division_id,
+                "type" => "Dossier Documents",
                 "parent_id" => $data->parent_id,
                 "parent_type" => $data->parent_type,
                 "short_description" => $data->short_description ? $data->short_description : "-",
@@ -741,6 +762,12 @@ class DashboardController extends Controller
             $data = Deviation::find($id);
             $single = "deviationSingleReport/". $data->id;
             $audit = "#";
+            $parent="deviationparentchildReport/". $data->id;
+        }
+        elseif ($type == "Dossier Documents") {
+            $data = DosierDocuments::find($id);
+            $single = "dosierdocuments/single_report/". $data->id;
+            $audit = "dosierdocuments/audit_report/". $data->id;
             $parent="deviationparentchildReport/". $data->id;
         }
 
