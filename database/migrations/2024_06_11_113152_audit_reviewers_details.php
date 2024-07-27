8c47b956bff3597142881ebfb788fa063737bbc3
sonali25sharma <sonalisharmavidyagxp@gmail.com>
2024-07-27 12:47:07 +0530

    duplicate This file audit_reviewers_details in nirvana project

--- database/migrations/2024_06_11_113152_audit_reviewers_details.php
+++ database/migrations/2024_06_11_113152_audit_reviewers_details.php
@@ -1,39 +1,39 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('audit_reviewers_details', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('deviation_id');
-            $table->unsignedBigInteger('user_id');
-            $table->text('type')->nullable();
-            $table->longText('reviewer_comment')->nullable();
-            $table->longText('reviewer_comment_by')->nullable();
-            $table->date('reviewer_comment_on')->nullable();
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         Schema::create('audit_reviewers_details', function (Blueprint $table) {
+//             $table->id();
+//             $table->unsignedBigInteger('deviation_id');
+//             $table->unsignedBigInteger('user_id');
+//             $table->text('type')->nullable();
+//             $table->longText('reviewer_comment')->nullable();
+//             $table->longText('reviewer_comment_by')->nullable();
+//             $table->date('reviewer_comment_on')->nullable();
 
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
+//             $table->timestamps();
+//             $table->softDeletes();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         //
+//     }
+// };
